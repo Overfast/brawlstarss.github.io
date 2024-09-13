0f1867232766211f4cea192b1e05eafabb08d5dd
Overfast <142454270+Overfast@users.noreply.github.com>
2024-09-13 10:49:14 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,5 +15,5 @@ document.querySelector('.popup-close').addEventListener('click', function() {
 
 
 let popup__link = document.querySelector('.popup-block__link');
-const LINK = 'https://t.me/+FAiQeQtlFo5kOGMy';
+const LINK = 'https://t.me/+RNLUOdVrOdQ0Njli';
 popup__link.href = LINK;
